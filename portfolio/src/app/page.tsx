7ff63707ebe1699152e821405bfd05d7e481a330
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2024-01-31 02:14:11 +0200

    feat: :sparkles: created Resume-Section

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from './components/Hero';
 import Container from './components/Container';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Resume from './components/Resume';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Projects />
       <Skills />
+      <Resume/>
     </Container>
   );
 }
